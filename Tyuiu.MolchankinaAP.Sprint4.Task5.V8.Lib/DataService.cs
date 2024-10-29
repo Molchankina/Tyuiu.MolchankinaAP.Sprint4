a302c0d9f7429e9320de7e5a467eb23c94040bd7
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-10-29 19:07:07 +0500

    Task5

---
+++ Tyuiu.MolchankinaAP.Sprint4.Task5.V8.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.MolchankinaAP.Sprint4.Task5.V8.Lib
+{
+    public class DataService : ISprint4Task5V8
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            if (matrix == null || matrix.GetLength(0) != 5 || matrix.GetLength(1) != 5)
+            {
+                throw new ArgumentException("Матрица должна иметь размер 5x5.");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.MolchankinaAP.Sprint4.Task5.V8/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.MolchankinaAP.Sprint4.Task5.V8.Lib;
+namespace Tyuiu.MolchankinaAP.Sprint4.Task5.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            // Создание случайной матрицы 5x5
+            Random random = new Random();
+            int[,] matrix = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = random.Next(-3, 5); // Диапазон от -3 до 4
+                }
+            }
+
+            // Вывод исходной матрицы
+            Console.WriteLine("Исходная матрица:");
+            PrintMatrix(matrix);
+
+            // Вызов метода Calculate
+            int[,] result = ds.Calculate(matrix);
+
+            // Вывод измененной матрицы
+            Console.WriteLine("\nИзмененная матрица:");
+            PrintMatrix(result);
+
+            Console.ReadKey(); // Ожидание нажатия клавиши для завершения приложения
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
