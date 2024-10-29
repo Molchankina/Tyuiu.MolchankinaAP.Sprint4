700eb1e6808d5fd6b603f46aeec9587303929e4c
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-10-29 17:55:57 +0500

    Task3

---
+++ Tyuiu.MolchankinaAP.Sprint4.Task3.V24.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.MolchankinaAP.Sprint4.Task3.V24.Lib
+{
+    public class DataService : ISprint4Task3V24
+    {
+        public int Calculate(int[,] array)
+        {
+            int minElement = int.MaxValue;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (array[i, 1] < minElement)
+                {
+                    minElement = array[i, 1];
+                }
+            }
+            return minElement;
+        }
+    }
+}
---
+++ Tyuiu.MolchankinaAP.Sprint4.Task3.V24/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.MolchankinaAP.Sprint4.Task3.V24.Lib;
+namespace Tyuiu.MolchankinaAP.Sprint4.Task3.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[,] array = {
+            { 3, 6, 1, 7, 3 },
+            { 2, 3, 7, 1, 1 },
+            { 1, 2, 5, 5, 1 },
+            { 7, 6, 7, 6, 2 },
+            { 7, 6, 4, 5, 8 }
+            };
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(array);
+            Console.WriteLine($"Минимальный элемент во втором столбце:" + res);
+            Console.ReadKey();
+        }
+    }
+}
